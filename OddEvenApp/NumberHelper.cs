eee9ef6d8a3b1dff673a0543ef58569bc4ac427e
Yousuf <yousufahmed3@proton.me>
2024-10-20 22:24:44 +0300

    Refactor IsEven and IsOdd methods to expression bodied method syntax

--- OddEvenApp/NumberHelper.cs
+++ OddEvenApp/NumberHelper.cs
@@ -8,14 +8,8 @@ namespace OddEvenApp
 {
     public static class NumberHelper
     {
-        public static bool IsEven(int number)
-        {
-            return number % 2 == 0;
-        }
+        public static bool IsEven(int number) => number % 2 == 0;
 
-        public static bool IsOdd(int number)
-        {
-            return !IsEven(number);
-        }
+        public static bool IsOdd(int number) => !IsEven(number);
     }
 }
